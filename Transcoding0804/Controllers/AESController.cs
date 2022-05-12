29d0a07f806d6fccce0331c33053146830b3cdb2
box9229@gmail.com <box9229@gmail.com>
2022-05-12 18:43:56 +0800

    Add files via upload

---
+++ Transcoding0804/Controllers/AESController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Transcoding0804.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+
+    public class CiphertController : ControllerBase
+    {
+        public string InvoiceNumber = "PL49821416";
+        public string RandomNumber = "5871";
+        public string AESKey = "F1A618ED8685B1A3B81E6CB6884617F4";
+        QREncrypter QREncrypter = new QREncrypter();
+        public string Ciphert = "";
+        // GET: api/<CiphertController>
+        [HttpGet]
+        //public IEnumerable<string> Get()
+        //return new string[] { InvoiceNumber, RandomNumber, AESKey };
+        public string Get()
+        {
+            return "https://ciper804.azurewebsites.net/Ciphert/PL49821416/5871/F1A618ED8685B1A3B81E6CB6884617F4";
+        }
+
+        // GET api/<CiphertController>/5
+        //[HttpGet("{IN}/{RN}/{Key}.{format?}")]
+        [HttpGet("{IN}/{RN}/{Key}")]
+        public string Get(string IN, string RN, string Key)
+        {
+            QREncrypter.InvoiceNumber = IN;
+            QREncrypter.RandomNumber = RN;
+            QREncrypter.AESKey = Key;
+            return QREncrypter.CiphertGet();
+        }
+        /*
+        private JsonResult JsonResult(string v)
+        {
+            throw new NotImplementedException();
+        }
+        */
+        // POST api/<CiphertController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<CiphertController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<CiphertController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Transcoding0804/QREncrypter.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.Text;
+using System.IO;
+namespace Transcoding0804
+{
+    public class QREncrypter
+    {
+        public string AESEncrypt(string plainText, string AESKey)
+        {
+            byte[] bytes = Encoding.Default.GetBytes(plainText);
+            ICryptoTransform transform = new RijndaelManaged
+            {
+                KeySize = 0x80,
+                Key = this.convertHexToByte(AESKey),
+                BlockSize = 0x80,
+                IV = Convert.FromBase64String("Dt8lyToo17X/XkXaQvihuA==")
+            }.CreateEncryptor();
+            MemoryStream stream = new MemoryStream();
+            CryptoStream stream2 = new CryptoStream(stream, transform, CryptoStreamMode.Write);
+            stream2.Write(bytes, 0, bytes.Length);
+            stream2.FlushFinalBlock();
+            stream2.Close();
+            return Convert.ToBase64String(stream.ToArray());
+        }
+        /// <summary>
+        /// 轉換HEX值為 Binaries
+        /// </summary>
+        /// <param name="hexString">HEX字串</param>
+        /// <returns>Binaries值</returns>
+        private byte[] convertHexToByte(string hexString)
+        {
+            byte[] buffer = new byte[hexString.Length / 2];
+            int index = 0;
+            for (int i = 0; i < hexString.Length; i += 2)
+            {
+                int num3 = Convert.ToInt32(hexString.Substring(i, 2), 0x10);
+                buffer[index] = BitConverter.GetBytes(num3)[0];
+                index++;
+            }
+            return buffer;
+        }
+        private void inputValidate(string InvoiceNumber,
+        //    string InvoiceDate,
+        //    string InvoiceTime,
+            string RandomNumber,
+        //    decimal SalesAmount,
+        //    decimal TaxAmount,
+        //    decimal TotalAmount,
+        //    string BuyerIdentifier,
+        //    string RepresentIdentifier,
+        //    string SellerIdentifier,
+        //    string BusinessIdentifier,
+        //    Array[] productArray,
+            string AESKey)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(InvoiceNumber) || (InvoiceNumber.Length != 10))
+                {
+                    throw new Exception("Invaild InvoiceNumber: " + InvoiceNumber);
+                }
+                if (string.IsNullOrEmpty(RandomNumber) || (RandomNumber.Length != 4))
+                {
+                    throw new Exception("Invaild RandomNumber: " + RandomNumber);
+                }
+                if (string.IsNullOrEmpty(AESKey) || (AESKey.Length != 32))
+                {
+                    throw new Exception("Invaild AESKey: " + AESKey);
+                }
+                /*
+                long num = long.Parse(InvoiceDate);
+                int num2 = int.Parse(InvoiceDate.Substring(3, 2));
+                int num3 = int.Parse(InvoiceDate.Substring(5));
+                if ((num2 < 1) || (num2 > 12))
+                {
+                    throw new Exception();
+                }
+                if ((num3 < 1) || (num3 > 0x1f))
+                {
+                    throw new Exception();
+                }
+                */
+            }
+            catch (Exception)
+            {
+                /*
+                if (string.IsNullOrEmpty(InvoiceDate))
+                {
+                    throw new Exception("Invaild InvoiceDate: " + InvoiceDate);
+                }
+                if (string.IsNullOrEmpty(InvoiceTime))
+                {
+                    throw new Exception("Invaild InvoiceTime: " + InvoiceTime);
+                }
+                if (SalesAmount < 0M)
+                {
+                    throw new Exception("Invaild SalesAmount: " + SalesAmount);
+                }
+                if (TotalAmount < 0M)
+                {
+                    throw new Exception("Invaild TotalAmount: " + TotalAmount);
+                }
+                if (string.IsNullOrEmpty(BuyerIdentifier) || (BuyerIdentifier.Length != 8))
+                {
+                    throw new Exception("Invaild BuyerIdentifier: " + BuyerIdentifier);
+                }
+                if (string.IsNullOrEmpty(RepresentIdentifier))
+                {
+                    throw new Exception("Invaild RepresentIdentifier: " + RepresentIdentifier);
+                }
+                if (string.IsNullOrEmpty(SellerIdentifier) || (SellerIdentifier.Length != 8))
+                {
+                    throw new Exception("Invaild SellerIdentifier: " + SellerIdentifier);
+                }
+                if (string.IsNullOrEmpty(BusinessIdentifier))
+                {
+                    throw new Exception("Invaild BusinessIdentifier: " + BusinessIdentifier);
+                }
+                if ((productArray == null) || (productArray.Length == 0))
+                {
+                    throw new Exception("Invaild ProductArray");
+                }
+                */
+                throw new Exception();
+            }
+        }
+        public string QRCodeINV(string InvoiceNumber,
+            //    string InvoiceDate,
+            //    string InvoiceTime,
+            string RandomNumber,
+            //    decimal SalesAmount,
+            //    decimal TaxAmount,
+            //    decimal TotalAmount,
+            //    string BuyerIdentifier,
+            //    string RepresentIdentifier,
+            //    string SellerIdentifier,
+            //    string BusinessIdentifier,
+            //    string[][] productArray,
+            string AESKey)
+        {
+            try
+            {
+                this.inputValidate(InvoiceNumber,
+                    //        InvoiceDate,
+                    //        InvoiceTime,
+                    RandomNumber,
+                    //        SalesAmount,
+                    //        TaxAmount,
+                    //        TotalAmount,
+                    //        BuyerIdentifier,
+                    //        RepresentIdentifier,
+                    //        SellerIdentifier,
+                    //        BusinessIdentifier,
+                    //        productArray,
+                    AESKey);
+            }
+            catch (Exception exception)
+            {
+                
+                if (string.IsNullOrEmpty(InvoiceNumber) || (InvoiceNumber.Length != 10))
+                {
+                    throw new Exception("Invaild RandomNumber: " + InvoiceNumber);
+                }
+                if (string.IsNullOrEmpty(RandomNumber) || (RandomNumber.Length != 4))
+                {
+                    throw new Exception("Invaild RandomNumber: " + RandomNumber);
+                }
+                if (string.IsNullOrEmpty(AESKey) || (AESKey.Length != 32))
+                {
+                    throw new Exception("Invaild AESKey: " + AESKey);
+                }
+
+                throw exception;
+            }
+            return this.AESEncrypt(InvoiceNumber + RandomNumber, AESKey).PadRight(0x18);
+        }
+        public string InvoiceNumber { get; set; }
+        public string RandomNumber { get; set; }
+        public string AESKey { get; set; }
+
+        public string CiphertGet() {
+          //return this.QRCodeINV(InvoiceNumber, RandomNumber, AESKey); 
+          return this.AESEncrypt(InvoiceNumber + RandomNumber, AESKey).PadRight(0x18);
+        }
+    }
+}
